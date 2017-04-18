570c7d7ba68e33ef940afde46fd8609fac1342d8
mit2nil <mit2nil@gmail.com>
2017-04-18 11:33:55 -0700

    Updated puam results based on ulan data

--- linkage/verify_relevance/museum_graph_api_config.py
+++ linkage/verify_relevance/museum_graph_api_config.py
@@ -1,3 +1,4 @@
 GM="http://data.americanartcollaborative.org/GM"
 IMA="http://data.americanartcollaborative.org/ima"
 WAM="http://data.americanartcollaborative.org/wam"
+PUAM="http://data.americanartcollaborative.org/puam"
