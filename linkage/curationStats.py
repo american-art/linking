ab6787facbe0272d20daf14d355feee1b2045fdc
mit2nil <mit2nil@gmail.com>
2017-05-10 16:27:51 -0700

    updates scripts to generate various statistics reported in the paper

--- linkage/curationStats.py
+++ linkage/curationStats.py
@@ -2,14 +2,17 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 from urllib2 import URLError
 import os, sys, json, time
 
-bases = ["autry","ccma","GM","ima","wam"]
+bases = ["aaa","acm","autry","cbm","ccma","dma","GM","ima","nmwa","npg","puam","saam","wam"]
+out = {"total":{}}
+f_in = open("curationStats.sparql", 'r')
+base_query = f_in.read()
+f_in.close()
 
 for base in bases:
     print ("Base is "+base)
-    f_in = open("curationStats.sparql", 'r')
     sparql = SPARQLWrapper("http://data.americanartcollaborative.org/sparql")
 
-    query = f_in.read().replace("???",base)
+    query = base_query.replace("???",base)
 
     #print (query)
 
@@ -17,54 +20,58 @@ for base in bases:
     sparql.setReturnFormat(JSON)
     sparql.setTimeout(360)
     while True:
-        print "Querying ",base," dataset"
+        #print "Querying ",base," dataset"
         try:
             results = sparql.query().convert()
             break
         except URLError:
             print("Connection to Sparql server failed! Trying again in five seconds!")
             time.sleep(5)
-        
-    f_in.close()
-    
-    TP = 0 # When raw ulan id match with curated ulan id
-    FP = 0 # When raw ulan id does not match with curated ulan id
-    FN = 0 # When raw ulan id is present but curated ulan id is missing
-    TN = 0 # When raw ulan id is missing but curated ulan id is present
+
+    TP = 0 # When raw ulan id match with curated ulan id (old-right)
+    FP = 0 # When raw ulan id does not match with curated ulan id (old-wrong/new-wrong)
+    FN = 0 # When raw ulan id is present but curated ulan id is missing (missed)
+    TN = 0 # When raw ulan id is missing but curated ulan id is present (new-right)
     for j in results["results"]["bindings"]:
         
         if "curated_ulan" in j and "raw_ulan" in j:
             if j["raw_ulan"]["value"] == j["curated_ulan"]["value"]:
                 TP += 1
             elif j["raw_ulan"]["value"] != j["curated_ulan"]["value"]:
+                print ("aac uri :"+j["uri"]["value"]+" raw ulan : "+j["raw_ulan"]["value"]+" curated ulan : "+j["curated_ulan"]["value"])
                 FP += 1
         elif "curated_ulan" in j and "raw_ulan" not in j:
             TN += 1
-        else:
+        elif "curated_ulan" not in j and "raw_ulan" in j:
             FN += 1
 
-    precision = (TP*1.0) / (TP + FP)
-    recall = (TP*1.0) / (TP + FN)
-    fscore = (2*precision*recall)/(precision+recall)
-    stats = {"counts":{"TP":TP, "FP": FP, "FN": FN, "TN": TN},
+    if (TP + FP ) > 0:
+        precision = (TP*1.0) / (TP + FP)
+    else:
+        precision = 0.0
+        
+    if (TP + FN) > 0:
+        recall = (TP*1.0) / (TP + FN)
+    else:
+        recall = 0.0
+    
+    if (precision+recall) > 0:
+        fscore = (2*precision*recall)/(precision+recall)
+    else:
+        fscore = 0.0
+        
+    stats = {"counts":{"old-right":TP, "old-wrong/new-wrong": FP, "missed": FN, "new-right": TN},
             "aggregates":{"Precision": precision, "Recall": recall, "F-Score": fscore}}
+    
     print (json.dumps(stats, indent=4, sort_keys=True))
-                    
-    # Save the results
-    '''
-    out = open('curationStats_'+base+'.json','w')
-    for j in results["results"]["bindings"]:
-        
-        o = {"uri":j["uri"]["value"]}
-        
-        if "raw_ulan" in j:
-            o["raw_ulan"] = j["raw_ulan"]["value"]
-        
-        if "curated_ulan" in j:
-            o["curated_ulan"] = j["curated_ulan"]["value"]
-        
-        out.write(json.dumps(o))
-        out.write("\n")
-        
-    out.close()
-    '''
+    
+    out[base] = stats
+    
+    for key in stats["counts"]:
+        if key in out["total"]:
+            out["total"][key] += stats["counts"][key]
+        else:
+            out["total"][key] = stats["counts"][key]
+
+print ("\nTotal \n")
+print (json.dumps(out["total"], indent=4, sort_keys=True))
---
+++ linkage/overallStats.py
@@ -0,0 +1,51 @@
+from SPARQLWrapper import SPARQLWrapper, JSON
+from urllib2 import URLError
+import os, sys, json, time
+
+classes = ["E39_Actor","E22_Man-Made_Object","E5_Event","E53_Place","triples","labels"]
+bases = ["aaa","acm","autry","cbm","ccma","dma","GM","ima","nmwa","npg","puam","saam","wam"]
+out = {"total":{}}
+f_in = open("overallStats.sparql", 'r')
+base_query = f_in.read()
+f_in.close()
+
+for b in bases:
+    print ("Base is "+b)
+    graph_query = base_query.replace("<graph>",b)
+    sparql = SPARQLWrapper("http://data.americanartcollaborative.org/sparql")
+    out_c = {}
+    
+    for c in classes:
+        if c == "triples":
+            query = graph_query.replace("?s a crm:<class>","?s ?o ?p")
+        elif c == "labels":
+            query = graph_query.replace("?s a crm:<class>;","?s a crm:E39_Actor;\n skos:exactMatch ?lod.")
+        else:
+            query = graph_query.replace("<class>",c)
+
+        #print (query)
+
+        sparql.setQuery(query)
+        sparql.setReturnFormat(JSON)
+        sparql.setTimeout(360)
+        while True:
+            try:
+                results = sparql.query().convert()
+                break
+            except URLError:
+                print("Connection to Sparql server failed! Trying again in five seconds!")
+                time.sleep(5)
+
+        out_c[c] = results["results"]["bindings"][0]["c"]["value"]
+
+    out[b] = out_c
+    print (json.dumps(out_c, indent=4, sort_keys=True))
+    
+    for key in out_c:
+        if key in out["total"]:
+            out["total"][key] += int(out_c[key])
+        else:
+            out["total"][key] = int(out_c[key])
+
+print ("\nTotal \n")
+print (json.dumps(out["total"], indent=4, sort_keys=True))
