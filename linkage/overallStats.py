05b12ebd389d69c8e0ae0dd055856fb75941448c
mit2nil <mit2nil@gmail.com>
2017-05-11 22:09:08 -0700

    Updated stats counting script to include all sub classes

--- linkage/overallStats.py
+++ linkage/overallStats.py
@@ -2,8 +2,23 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 from urllib2 import URLError
 import os, sys, json, time
 
-classes = ["E39_Actor","E22_Man-Made_Object","E12_Production","E53_Place","triples","labels"]
-bases = ["aaa","acm","autry","cbm","ccma","dma","GM","ima","nmwa","npg","puam","saam","wam"]
+classes = ["triples","labels"]
+bases = ["aaa","acm","autry","cbm","ccma","dma","GM","ima","nmwa","npg","puam","saam","wam","YCBA"]
+
+constituents = ["E21_Person","E39_Actor","E40_Legal_Body","E74_Group"]
+objects = ["E22_Man-Made_Object","E84_Information_Carrier"]
+events = ["E5_Event","E6_Destruction","E7_Activity","E8_Acquisition","E9_Move","E10_Transfer_of_Custody",
+                "E11_Modification","E12_Production","E13_Attribute_Assignment","E14_Condition_Assessment",
+                "E15_Identifier_Assignment","E16_Measurement","E17_Type_Assignment","E79_Part_Addition",
+                "E80_Part_Removal","E63_Beginning_of_Existence","E64_End_of_Existence","E65_Creation",
+                "E66_Formation","E67_Birth","E68_Dissolution","E69_Death","E81_Transformation","E83_Type_Creation",
+                "E85_Joining","E86_Leaving","E87_Curation_Activity"]
+places = ["E53_Place"]
+
+# All classes
+all_classes = []
+all_classes = classes + all_classes + constituents + objects + events + places
+
 out = {"total":{}}
 f_in = open("overallStats.sparql", 'r')
 base_query = f_in.read()
@@ -13,9 +28,9 @@ for b in bases:
     print ("Base is "+b)
     graph_query = base_query.replace("<graph>",b)
     sparql = SPARQLWrapper("http://data.americanartcollaborative.org/sparql")
-    out_c = {}
+    out_c = {"constituents":0,"objects":0,"events":0,"places":0}
     
-    for c in classes:
+    for c in all_classes:
         if c == "triples":
             query = graph_query.replace("?s a crm:<class>","?s ?o ?p")
         elif c == "labels":
@@ -36,16 +51,25 @@ for b in bases:
                 print("Connection to Sparql server failed! Trying again in five seconds!")
                 time.sleep(5)
 
-        out_c[c] = results["results"]["bindings"][0]["c"]["value"]
+        if c in constituents:
+            out_c["constituents"] += int(results["results"]["bindings"][0]["c"]["value"])
+        elif c in objects:
+            out_c["objects"] += int(results["results"]["bindings"][0]["c"]["value"])
+        elif c in events:
+            out_c["events"] += int(results["results"]["bindings"][0]["c"]["value"])
+        elif c in places:
+            out_c["places"] += int(results["results"]["bindings"][0]["c"]["value"])
+        else:
+            out_c[c] = int(results["results"]["bindings"][0]["c"]["value"])
 
     out[b] = out_c
     print (json.dumps(out_c, indent=4, sort_keys=True))
     
     for key in out_c:
         if key in out["total"]:
-            out["total"][key] += int(out_c[key])
+            out["total"][key] += out_c[key]
         else:
-            out["total"][key] = int(out_c[key])
+            out["total"][key] = out_c[key]
 
 print ("\nTotal \n")
 print (json.dumps(out["total"], indent=4, sort_keys=True))
